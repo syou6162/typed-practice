eda929d5dc9029050dd5c0629fdd74e3bbd6b10e
Yasuhisa Yoshida <syou6162@gmail.com>
2013-06-12 18:30:33 +0900

    updated project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject typed-practice "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject typed-practice "0.0.1"
+  :description "core.typedの練習"
+  :url "https://github.com/syou6162/typed-practice"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
